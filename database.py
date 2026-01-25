9b3c88530c7f31b15fce1a217c63a947bdcc9f41
mlapusta <lapusta.mariia@gmail.com>
2026-01-25 14:18:14 +0100

    Add Expense class

---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, DateTime
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in .env file")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[int] = mapped_column(Integer)
+    category: Mapped[str] = mapped_column(String)
+    date: Mapped[datetime] = mapped_column(DateTime)
+    user_id: Mapped[str] = mapped_column(String)
