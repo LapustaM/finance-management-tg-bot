c44444879be51b216fb6201a8364c953531e145e
mlapusta <lapusta.mariia@gmail.com>
2026-01-25 18:00:02 +0100

    Add func to show plot

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL.ImageOps import expand
+
+
 def parse_message(message: str):
     parts = message.split(' ', maxsplit=1)
     if len(parts) > 2:
@@ -10,3 +15,13 @@ def parse_message(message: str):
 
     amount = int(amount_str)
     return amount, category.strip().capitalize()
+
+def create_pie_chart(expenses):
+    amounts = []
+    categories = []
+    for category, amount in expenses:
+        categories.append(category)
+        amounts.append(amount)
+    colors = sns.color_palette("pastel")
+    plt.pie(amounts, labels=categories, colors=colors, autopct='%.0f%%')
+    plt.show()
