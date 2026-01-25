48172a0f5110369e53014e7c6da5de4e9507f5f2
mlapusta <lapusta.mariia@gmail.com>
2026-01-25 15:27:18 +0100

    Add get total expense for month

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer, DateTime, select, func
+from sqlalchemy import String, Integer, DateTime, select, func, extract
 from dotenv import load_dotenv
 import os
 from datetime import datetime
@@ -33,12 +33,12 @@ async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-async def add_expense(amount: int, category: str, user_id: str):
+async def add_expense(amount: int, category: str, user_id: str, date=datetime.now()):
     async with new_session() as session:
         expense = Expense(
             amount=amount,
             category=category,
-            date=datetime.now(),
+            date=date,
             user_id=user_id
         )
         session.add(expense)
@@ -47,6 +47,13 @@ async def add_expense(amount: int, category: str, user_id: str):
 async def get_total_expenses(user_id: str) -> int:
     async with new_session() as session:
         query = select(func.sum(Expense.amount)).where(Expense.user_id == user_id)
+        result = await session.execute(query)
+        total = result.scalar()
+        return total if total else 0
+
+async def get_total_expenses_month(user_id: str, month: int, year: int):
+    async with new_session() as session:
+        query = select(func.sum(Expense.amount)).where(Expense.user_id == user_id).filter(extract('month', Expense.date) == month).filter(extract('year', Expense.date) == year)
         result = await session.execute(query)
         total = result.scalar()
         return total if total else 0
